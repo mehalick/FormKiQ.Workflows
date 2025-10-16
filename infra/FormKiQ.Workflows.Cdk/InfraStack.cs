7e7cb7b1a58a31b5b28fbf5e7c36a68c17ac0e56
Andy Mehalick <amhlck@amazon.com>
2025-10-16 07:58:58 -0400

    init cdk project

---
+++ infra/FormKiQ.Workflows.Cdk/InfraStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace FormKiQ.Workflows.Cdk;
+
+public class InfraStack : Stack
+{
+    internal InfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+    {
+        // The code that defines your stack goes here
+    }
+}
