dda16f6327ddd16cd3cf2f89abc4745e325e285e
Andy Mehalick <amhlck@amazon.com>
2025-10-16 08:46:42 -0400

    add user secrets configuration

--- infra/FormKiQ.Workflows.Cdk/InfraStack.cs
+++ infra/FormKiQ.Workflows.Cdk/InfraStack.cs
@@ -1,12 +1,24 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.SNS;
+using Amazon.CDK.AWS.SNS.Subscriptions;
+using Amazon.CDK.AWS.SQS;
 using Constructs;
+using Microsoft.Extensions.Configuration;
 
 namespace FormKiQ.Workflows.Cdk;
 
 public class InfraStack : Stack
 {
-    internal InfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+    internal InfraStack(Construct scope, string id, IConfiguration configuration, IStackProps props = null) : base(scope, id, props)
     {
-        // The code that defines your stack goes here
+        var existingTopic = Topic.FromTopicArn(this, "SnsDocumentEventTopic", configuration["AWS:SnsTopicArn"]!);
+        var queue = CreateQueueWithSnsSubscription("DocumentEventQueue", existingTopic);
+    }
+
+    private Queue CreateQueueWithSnsSubscription(string queueName, ITopic snsTopic)
+    {
+        var queue = new Queue(this, queueName);
+        snsTopic.AddSubscription(new SqsSubscription(queue));
+        return queue;
     }
 }
