caf76b08d6ae85fc7e03f3715229364cf4d49372
Andy Mehalick <andy.mehalick@outlook.com>
2025-12-30 08:20:10 -0500

    update user secret names

--- cdk/FormKiQ.Cdk/Program.cs
+++ cdk/FormKiQ.Cdk/Program.cs
@@ -16,11 +16,11 @@ internal abstract class Program
 
         var props = new InfraStackProps("formkiq-workflows")
         {
-            FormKiqBaseUrl = configuration["FormKiQ:BaseUrl"] ?? throw new("FormKiQ:BaseUrl is missing"),
-            FormKiQApiKey = configuration["FormKiQ:ApiKey"] ??  throw new("FormKiQ:ApiKey is missing"),
-            SlackWebhookUrl = configuration["Slack:WebhookUrl"] ??  throw new("Slack:WebhookUrl is missing"),
-            SnsTopicArn = configuration["AWS:SnsTopicArn"] ??  throw new("AWS:SnsTopicArn is missing"),
-            S3BucketName = configuration["AWS:S3BucketName"] ??  throw new("AWS:S3BucketName is missing")
+            FormKiqBaseUrl = configuration["FORMKIQ__BASEURL"] ?? throw new("FormKiQ:BaseUrl is missing"),
+            FormKiQApiKey = configuration["FORMKIQ__APIKEY"] ??  throw new("FormKiQ:ApiKey is missing"),
+            SlackWebhookUrl = configuration["SLACK__WEBHOOKURL"] ??  throw new("Slack:WebhookUrl is missing"),
+            SnsTopicArn = configuration["AWS__SNSTOPICARN"] ??  throw new("AWS:SnsTopicArn is missing"),
+            S3BucketName = configuration["AWS__S3BUCKETNAME"] ??  throw new("AWS:S3BucketName is missing")
         };
 
         _ = new InfraStack(app, props.StackId, props);
